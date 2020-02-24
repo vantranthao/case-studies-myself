0d750257b44467092588e002c488b6d6a8123246
vantranthao <tranthaovan2501@gmail.com>
2020-02-24 16:22:01 +0700

    hex-color

---
+++ change-hex-bg-color/index.js
@@ -0,0 +1,15 @@
+var showMe = document.getElementById('show-me');
+var clickMe = document.getElementById('click-me');
+
+const changeColor = () => {
+    var hexColor = ['a', 'b', 'c', 'e', 'd', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    let hex = '#';
+    for(let i = 0 ; i < 6; i++) {
+        let index = Math.floor(Math.random()*hexColor.length);
+        hex += hexColor[index];
+    }
+    document.body.style.background = hex;
+    showMe.innerHTML = hex;
+}
+
+clickMe.addEventListener('click', changeColor);
